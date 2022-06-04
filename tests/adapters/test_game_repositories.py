4b225abe199ac39d087fe8ddc2bdae85555f123a
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-04 23:07:48 +0300

    Extract reusable parts of tests for adapters.

---
+++ tests/adapters/test_game_repositories.py
@@ -0,0 +1,54 @@
+from hypothesis import given, settings
+from hypothesis.strategies import text
+from moto import mock_dynamodb2
+from pytest import raises
+
+from paper_tactics.adapters.in_memory_game_repository import InMemoryGameRepository
+from paper_tactics.ports.game_repository import NoSuchGameException
+from tests.adapters.strategies import dynamodb_game_repositories
+from tests.entities.strategies import games
+
+
+def _test_game_is_not_changed_if_written_and_read_back(game_repository, game):
+    game_repository.store(game)
+
+    assert game_repository.fetch(game.id) == game
+
+
+def _test_no_such_game_exception_is_thrown_if_game_does_not_exist(
+    game_repository, game_id
+):
+    with raises(NoSuchGameException):
+        game_repository.fetch(game_id)
+
+
+@mock_dynamodb2
+@given(dynamodb_game_repositories(), games())
+@settings(max_examples=5)
+def test_game_is_not_changed_if_written_and_read_back_in_dynamodb(
+    game_repository, game
+):
+    _test_game_is_not_changed_if_written_and_read_back(game_repository, game)
+
+
+@mock_dynamodb2
+@given(dynamodb_game_repositories(), text())
+@settings(max_examples=10)
+def test_no_such_game_exception_is_thrown_if_game_does_not_exist_in_dynamodb(
+    game_repository, game_id
+):
+    _test_no_such_game_exception_is_thrown_if_game_does_not_exist(
+        game_repository, game_id
+    )
+
+
+@given(games())
+def test_game_is_not_changed_if_written_and_read_back_in_memory(game):
+    _test_game_is_not_changed_if_written_and_read_back(InMemoryGameRepository(), game)
+
+
+@given(text())
+def test_no_such_game_exception_is_thrown_if_game_does_not_exist_in_memory(game_id):
+    _test_no_such_game_exception_is_thrown_if_game_does_not_exist(
+        InMemoryGameRepository(), game_id
+    )
---
+++ tests/adapters/test_match_request_queues.py
@@ -0,0 +1,43 @@
+from hypothesis import given, settings
+from moto import mock_dynamodb2
+
+from tests.adapters.strategies import (
+    dynamodb_match_request_queues,
+    redis_match_request_queues,
+)
+from tests.entities.strategies import match_requests
+
+
+def _test_pop_on_empty_queue_returns_none(queue):
+    assert queue.pop() is None
+
+
+def _test_request_is_popped_after_stored_and_read_back(queue, request):
+    queue.put(request)
+
+    assert queue.pop() == request
+    assert queue.pop() is None
+
+
+@mock_dynamodb2
+@given(dynamodb_match_request_queues())
+@settings(max_examples=10)
+def test_pop_on_empty_queue_returns_none_in_dynamodb(queue):
+    _test_pop_on_empty_queue_returns_none(queue)
+
+
+@mock_dynamodb2
+@given(dynamodb_match_request_queues(), match_requests())
+@settings(max_examples=10)
+def test_request_is_popped_after_stored_and_read_back_in_dynamodb(queue, request):
+    _test_request_is_popped_after_stored_and_read_back(queue, request)
+
+
+@given(redis_match_request_queues())
+def test_pop_on_empty_queue_returns_none_in_redis(queue):
+    _test_pop_on_empty_queue_returns_none(queue)
+
+
+@given(redis_match_request_queues(), match_requests())
+def test_request_is_popped_after_stored_and_read_back(queue, request):
+    _test_request_is_popped_after_stored_and_read_back(queue, request)
