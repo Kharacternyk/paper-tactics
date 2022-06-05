5dc79783711a802d985fb2518a1c8d07370c228b
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-05 18:00:57 +0300

    Move the game view logic into the entities layer.

--- paper_tactics/adapters/aws_api_gateway_player_notifier.py
+++ paper_tactics/adapters/aws_api_gateway_player_notifier.py
@@ -1,10 +1,7 @@
-import json
-from typing import Any
-
 import boto3
 
 from paper_tactics.entities.game import Game
-from paper_tactics.entities.player import Player
+from paper_tactics.entities.game_view import GameView
 from paper_tactics.ports.player_notifier import PlayerGoneException, PlayerNotifier
 
 
@@ -15,50 +12,12 @@ class AwsApiGatewayPlayerNotifier(PlayerNotifier):
         )
 
     def notify(self, player_id: str, game: Game) -> None:
-        view = self._create_game_view(player_id, game)
+        view = GameView(game, player_id)
 
         try:
             self._client.post_to_connection(
-                Data=json.dumps(view, separators=(",", ":")).encode(),
+                Data=view.to_json().encode(),
                 ConnectionId=player_id,
             )
         except self._client.exceptions.GoneException:
             raise PlayerGoneException(player_id)
-
-    def _create_game_view(self, player_id: str, game: Game) -> dict[str, Any]:
-        if player_id == game.active_player.id:
-            return self._create_game_view_for_active_player(game)
-        elif player_id == game.passive_player.id:
-            return self._create_game_view_for_passive_player(game)
-
-        raise ValueError("No such player")
-
-    def _create_game_view_for_active_player(self, game: Game) -> dict[str, Any]:
-        return {
-            "id": game.id,
-            "size": game.size,
-            "myTurn": True,
-            "turnsLeft": game.turns_left,
-            "me": self._create_player_view(game.active_player),
-            "opponent": self._create_player_view(game.passive_player),
-        }
-
-    def _create_game_view_for_passive_player(self, game: Game) -> dict[str, Any]:
-        return {
-            "id": game.id,
-            "size": game.size,
-            "myTurn": False,
-            "turnsLeft": game.turns_left,
-            "me": self._create_player_view(game.passive_player),
-            "opponent": self._create_player_view(game.active_player),
-        }
-
-    def _create_player_view(self, player: Player) -> dict[str, Any]:
-        return {
-            "units": list(player.units),
-            "walls": list(player.walls),
-            "reachable": list(player.reachable),
-            "isGone": player.is_gone,
-            "isDefeated": player.is_defeated,
-            "viewData": player.view_data,
-        }
---
+++ paper_tactics/entities/game_view.py
@@ -0,0 +1,58 @@
+import json
+from dataclasses import asdict, dataclass
+
+from paper_tactics.entities.cell import Cell
+from paper_tactics.entities.game import Game
+from paper_tactics.entities.player import Player
+
+
+@dataclass
+class PlayerView:
+    units: set[Cell]
+    walls: set[Cell]
+    reachable: set[Cell]
+    view_data: dict[str, str]
+    is_gone: bool
+    is_defeated: bool
+
+    def __init__(self, player: Player):
+        self.units = player.units
+        self.walls = player.walls
+        self.reachable = player.reachable
+        self.is_gone = player.is_gone
+        self.is_defeated = player.is_defeated
+        self.view_data = player.view_data
+
+
+@dataclass
+class GameView:
+    id: str
+    size: int
+    turns_left: int
+    my_turn: bool
+    me: PlayerView
+    opponent: PlayerView
+
+    def __init__(self, game: Game, player_id: str):
+        self.id = game.id
+        self.size = game.size
+        self.turns_left = game.turns_left
+
+        if player_id == game.active_player.id:
+            self.my_turn = True
+            self.me = PlayerView(game.active_player)
+            self.opponent = PlayerView(game.passive_player)
+        elif player_id == game.passive_player.id:
+            self.my_turn = False
+            self.me = PlayerView(game.passive_player)
+            self.opponent = PlayerView(game.active_player)
+        else:
+            raise ValueError("No such player")
+
+    def to_json(self) -> str:
+        game_dict = asdict(self)
+        for player_view in game_dict["me"], game_dict["opponent"]:
+            for key in player_view:
+                if isinstance(player_view[key], set):
+                    player_view[key] = list(player_view[key])
+        return json.dumps(game_dict, separators=(",", ":"))
