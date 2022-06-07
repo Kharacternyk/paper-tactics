e7f58826325d53d935976540e4b499a289b705e8
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-07 21:12:50 +0300

    Add a test for the GameView class.

---
+++ tests/entities/test_game_view.py
@@ -0,0 +1,12 @@
+import json
+
+from hypothesis import given
+
+from paper_tactics.entities.game_view import GameView
+from tests.entities.strategies import games
+
+
+@given(games())
+def test_game_views_are_json_serializable(game):
+    for player_id in game.active_player.id, game.passive_player.id:
+        json.loads(GameView(game, player_id).to_json())
