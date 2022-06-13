46a285a61ed1698d9fdbc39df17ffc6b84b201ae
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-13 21:41:54 +0300

    Make the bot a bit smarter.

--- paper_tactics/entities/game_bot.py
+++ paper_tactics/entities/game_bot.py
@@ -1,9 +1,17 @@
-from random import choice
+from dataclasses import dataclass
+from random import choices
 
 from paper_tactics.entities.cell import Cell
 from paper_tactics.entities.game_view import GameView
 
 
+@dataclass
 class GameBot:
+    opponent_unit_weight: int = 7
+
     def make_turn(self, game_view: GameView) -> Cell:
-        return choice(list(game_view.me.reachable))
+        weights = [
+            1 if cell not in game_view.opponent.units else self.opponent_unit_weight
+            for cell in game_view.me.reachable
+        ]
+        return choices(list(game_view.me.reachable), weights)[0]
