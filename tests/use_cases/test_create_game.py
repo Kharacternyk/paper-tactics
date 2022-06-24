228ea59a795455fef44e46d16e86ec9378f7acac
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-24 21:39:49 +0300

    Drop unnecessary test case restrictions.

--- tests/use_cases/test_create_game.py
+++ tests/use_cases/test_create_game.py
@@ -1,4 +1,4 @@
-from hypothesis import given, settings
+from hypothesis import given
 
 from paper_tactics.entities.match_request import MatchRequest
 from paper_tactics.use_cases.create_game import create_game
@@ -13,7 +13,6 @@ from tests.use_cases.strategies import match_request_queues, player_notifiers
 
 
 @given(match_request_queues(), player_notifiers(), match_requests())
-@settings(max_examples=5)
 def test_game_is_stored_only_if_no_players_are_gone(
     match_request_queue: MockedMatchRequestQueue,
     player_notifier: MockedPlayerNotifier,
@@ -31,7 +30,6 @@ def test_game_is_stored_only_if_no_players_are_gone(
 
 
 @given(match_request_queues(), player_notifiers(), match_requests())
-@settings(max_examples=5)
 def test_players_are_notified_if_a_game_is_created(
     match_request_queue: MockedMatchRequestQueue,
     player_notifier: MockedPlayerNotifier,
